2fb49ea2bb96ba40e8b0a64d1876c38020d44213
DOVAN888 <144321116+DOVAN888@users.noreply.github.com>
2026-04-26 20:02:36 +0900

    fix: router + dependency conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignInPage from "./pages/SignInPage";
 import ChatAppPage from "./pages/ChatAppPage";
 import { Toaster } from "sonner";
